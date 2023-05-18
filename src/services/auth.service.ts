d6fe07c8a235238f6f5d045a87c4cb06080a85e6
PleBea <bini060315kim@gmail.com>
2023-05-19 08:38:50 +0900

    feat: Create auth service interface

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,1 +1,8 @@
-// TODO
+import { IUUID, User } from 'src/generated/auth.proto';
+
+export interface AuthService {
+  getUserByUuid(request: IUUID): Promise<User>;
+  createAccessTokenByUuid(request: IUUID): Promise<string>;
+  getAccessTokenIsValid(request: string): Promise<User>;
+  createUser(request: User): Promise<User>;
+}
