47f17588336a10d807c349ce855686243a862ffd
PleBea <bini060315kim@gmail.com>
2023-07-04 09:00:05 +0900

    feat: Create board proto, grpc option

---
+++ src/options/board.option.ts
@@ -0,0 +1,14 @@
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { addReflectionToGrpcConfig } from 'nestjs-grpc-reflection';
+
+export const serviceHost = '0.0.0.0';
+export const servicePort = 10003;
+
+export const grpcClientOptions: GrpcOptions = addReflectionToGrpcConfig({
+  transport: Transport.GRPC,
+  options: {
+    url: `${serviceHost}:${servicePort}`,
+    package: 'board',
+    protoPath: 'src/board.proto',
+  },
+});
